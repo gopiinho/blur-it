ab745bcf3737791a10ec8af8a8307701ed09ac93
Gurpreet <gs109690@gmail.com>
2025-05-01 23:48:00 +0530

    ✨ Image upload in a state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
-import Link from 'next/link'
+'use client'
+import { useRef } from 'react'
+import { useRouter } from 'next/navigation'
 import { MdBlurOn } from 'react-icons/md'
 import { RiImageAddFill } from 'react-icons/ri'
+import { useEditorStore } from '@/state/editorState'
 
 export default function Home() {
+  const fileInputRef = useRef<HTMLInputElement | null>(null)
+  const router = useRouter()
+  const { setImage } = useEditorStore()
+
+  const handleClick = () => {
+    fileInputRef.current?.click()
+  }
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+    if (!file) return
+
+    const reader = new FileReader()
+    reader.onload = () => {
+      const img = new Image()
+      img.onload = () => {
+        setImage(img)
+        router.push('/editor')
+      }
+      img.src = reader.result as string
+    }
+    reader.readAsDataURL(file)
+  }
+
   return (
     <div className="flex h-screen w-full flex-col items-center">
       <div className="relative h-[70%] w-full overflow-hidden py-20">
@@ -10,15 +37,23 @@ export default function Home() {
           <MdBlurOn size={50} />
           Blur It
         </h1>
-        <Link href={'/editor'}>
-          <div className="group text-background absolute bottom-10 left-1/2 flex h-60 w-60 -translate-x-1/2 cursor-pointer flex-col items-center justify-center italic duration-300">
-            <div className="bg-primary/90 absolute inset-0 z-0 rounded-full transition-transform duration-300 group-hover:scale-120" />
-            <div className="z-10 flex flex-col items-center justify-center">
-              <RiImageAddFill size={80} />
-              <span>Add Image</span>
-            </div>
+        <button
+          onClick={handleClick}
+          className="group text-background absolute bottom-10 left-1/2 flex h-60 w-60 -translate-x-1/2 cursor-pointer flex-col items-center justify-center italic duration-300"
+        >
+          <div className="bg-primary/90 absolute inset-0 z-0 rounded-full transition-transform duration-300 group-hover:scale-120" />
+          <div className="z-10 flex flex-col items-center justify-center">
+            <RiImageAddFill size={80} />
+            <span>Add Image</span>
           </div>
-        </Link>
+        </button>
+        <input
+          type="file"
+          accept="image/*"
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          className="hidden"
+        />
       </div>
     </div>
   )
